2fbc5d0e7bcd5269b01fb5389e52b1e68d40026b
MateusP21 <mateuspasso@outlook.com>
2021-11-29 18:23:59 -0300

    Exercicio 2 - Resolvido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-loop-for/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-loop-for/exercicios.js
@@ -3,5 +3,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // Exercicio - 1
 
 for (let index = 0; index < numbers.length; index++) {
-  console.log(numbers);
+  console.log(numbers[index]);
 }
+
+// Exercicio - 2
+let resultado = 0;
+for (let index = 0; index < numbers.length; index++) {
+  resultado += numbers[index];
+}
+
+console.log(resultado);
