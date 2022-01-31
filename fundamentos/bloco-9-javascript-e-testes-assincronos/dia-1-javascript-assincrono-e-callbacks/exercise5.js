c039d6d63403d80bf51ded0637be94885d2b0632
MateusP21 <mateuspasso@outlook.com>
2022-01-31 18:29:21 -0300

    implementando callbacks na function sendMarsTemperature

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise5.js
@@ -0,0 +1,31 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const sendMarsTemperature = (callback) => {
+  const temperature = getMarsTemperature();
+  if (!callback) {
+    const message = `Mars temperature is: ${temperature} degree Celsius`;
+    setTimeout(() => console.log(message), messageDelay());
+  }
+
+  setTimeout(() => console.log(callback(temperature)), messageDelay());
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9) / 5 + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(
+    `Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`
+  );
+
+// definição da função sendMarsTemperature...
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
