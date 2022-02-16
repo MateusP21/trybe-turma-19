7d1758be5b0a1e6d49bc87c5fb215c359342db73
MateusP21 <mateuspasso@outlook.com>
2022-02-16 19:13:05 -0300

    finalizando primeiro exercicio

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/meu-primeiro-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/meu-primeiro-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return <li>{value}</li>;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const tasks = [
+    'Verificar email',
+    'Ver a agenda do dia',
+    'Estudar o conteudo',
+    'Ir para aula ao vivo',
+    'Fazer os exercicios',
+  ];
+  return <ul>{tasks.map((item) => Task(item))}</ul>;
 }
 
 export default App;
